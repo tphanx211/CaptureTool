79917b853d85a331f3b00255e5687995abfc38e5
Tony Phan <tonyyhphann@gmail.com>
2025-04-20 05:23:18 -0500

    First draft of layout, top bar, window, logo, and buttons

---
+++ source/CaptureTool/Views/MainWindowOld.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CaptureTool.Views;
+
+public partial class MainWindowOld : Window
+{
+    public MainWindowOld()
+    {
+        InitializeComponent();
+    }
+}
